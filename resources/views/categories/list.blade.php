0cf051a928eae4368049afab0b5c13866238b75b
= <157025874+benmensah30@users.noreply.github.com>
2024-05-25 13:08:08 +0100

    lara1

--- resources/views/categories/list.blade.php
+++ resources/views/categories/list.blade.php
@@ -10,17 +10,29 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-xl-3">
+        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut rerum nam architecto tempora quidem quam quos cum beatae a nemo voluptates atque, nesciunt hic excepturi accusantium. Tempore excepturi magnam in!
         </div>
-        <div class="col-xl-3">
+        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut rerum nam architecto tempora quidem quam quos cum beatae a nemo voluptates atque, nesciunt hic excepturi accusantium. Tempore excepturi magnam in!
         </div>
-        <div class="col-xl-3">
+        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut rerum nam architecto tempora quidem quam quos cum beatae a nemo voluptates atque, nesciunt hic excepturi accusantium. Tempore excepturi magnam in!
         </div>
-        <div class="col-xl-3">
+        <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut rerum nam architecto tempora quidem quam quos cum beatae a nemo voluptates atque, nesciunt hic excepturi accusantium. Tempore excepturi magnam in!
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              </div>
         </div>
     </div>
 </div>
